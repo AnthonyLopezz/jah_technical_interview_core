0a9117ab9b7ce23d70b32bdd5c751971fa71f6ac
AnthonyLopezz <anthony.lopez@usantoto.edu.co>
2025-10-18 09:09:49 -0500

    chore(repo): configuración base y linters

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: { node: true, es2020: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  rules: {
+    'prettier/prettier': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
+  ignorePatterns: ['dist', 'node_modules'],
+};
